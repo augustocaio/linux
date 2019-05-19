8748b762c03620f19cf3f4f51042e47634cd8e58
elton-okawa <elton_okawa@hotmail.com>
2019-05-19 19:03:46 -0300

    fix: syscall declaration

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -910,6 +910,6 @@ asmlinkage long sys_stack_pop(void);
 asmlinkage long sys_hello_world(void);
 asmlinkage long sys_stop_process(int pid);
 asmlinkage long sys_continue_process(void);
-asmlinkage long sys_save(int number);
+asmlinkage long sys_save(long number);
 asmlinkage long sys_recover(void);
 #endif
